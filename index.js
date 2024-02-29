0670a21bd541ea6eed5b25fe966d557bf442a650
Kathiravanmohan <kathiravanmohan19@gmail.com>
2024-02-29 20:36:56 +0530

    aws-Backend

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ import { MongoClient } from 'mongodb';
 import * as dotenv  from "dotenv";
 import cors from 'cors';
 
+import router from './Auth/login.cjs';
+//
 
 dotenv.config();
 const app =  express()
 const PORT=8000
 
+app.use(express.json())
+app.use('/login', router)
 app.use(cors())
 console.log(process.env.Mongo_Url)
-const MongoUrl=process.env.Mongo_Url
+
 async function createConnections() {
+    const MongoUrl=process.env.Mongo_Url
     const client = new MongoClient(MongoUrl);
      await client.connect()
      console.log("Mongodb is connected")
@@ -39,5 +44,25 @@ app.post('/',async(req,res)=>{
 
  res.send(repository)
 })
+
+app.get('/item',async(req,res)=>{
+    const item=await client.db("AWS")
+    .collection("item")
+    .find().toArray();
+   
+    res.send(item)
+   });
+
+app.post('/item',async(req,res)=>{
+    const newitem=req.body
+    const item=await client.db("AWS")
+    .collection("item")
+    .insertMany(newitem);
+    
+
+ res.send(item)
+})
+
+
 
 app.listen(PORT,()=>console.log("app is listening in port", PORT))
